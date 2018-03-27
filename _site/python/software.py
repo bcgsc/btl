b8e67b5e0d3598c83f1d298cba772eafe7591b2e
Jessica Ma <gejessicama@gmail.com>
2018-03-27 15:18:33 -0700

    docker image, bioconda, linuxbrew for software

--- _site/python/software.py
+++ _site/python/software.py
@@ -3,32 +3,47 @@ import glob, os
 os.chdir("../softwareblurbs")
 software = open("../_includes/software-content.html", "w+")
 
-start = '<td><img class="software-logo" src="assets/logos/'
+start = '<td><img class="software-logo" src="../assets/logos/'
 p1 = '"><span class="software-title">'
 p2 = ' <a href="'
-p3 = '"><img class="git" src="assets/githubicon.svg"></a></span><p>'
+p3 = '"><img class="git" src="../assets/githubicon.svg"></a></span>'
+
+ds = '<div style="text-align:center;"><div class="downloadinfo">'
+d1 = '<br>{% include linuxbrew-icon.html%}'
+d2 = '<br>{% include bioconda-icon.html%}'
+d3 = '<br>{% include docker-icon.html%}'
+de = '</div></div>'
+
+p4 = '<p style="padding-top:0">'
 end= '</p></td>'
 
 i = 0
+
+def downloads(lin, bio, dock):
+    if not lin and not bio and not dock: return '' 
+    else: return ds + ((d1 + lin) if lin else '') + ((d2 + bio) if bio else '') + ((d3 + dock) if dock else '') + de
 
 for myfile in sorted(glob.glob("*.txt")):
     f = open(myfile)
 
     info = []	
-    for x in range(0,5):
+    for x in range(0,8):
 	    info.append(f.readline().strip())
-	    if x == 5: 
+	    if x == 8: 
 		    info.append(f.read().strip())
 
     logofile = info[0]
-    github = info[1]
-    name = info[2]
-    desc = info[3]
+    name = info[1]
+    github = info[2]
+    linuxbrew = info[3]
+    bioconda = info[4]
+    docker = info[5]
+    desc = info[6]
 
     if i%2 is 0: 
         software.write("<tr>")
 
-    html = start + logofile + p1 + name + p2 + github + p3 + desc + end
+    html = start + logofile + p1 + name + p2 + github + p3 + downloads(linuxbrew, bioconda, docker) + p4 + desc + end
     software.write(html)
     
     if i%2 is 1:
