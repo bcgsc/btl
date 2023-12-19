5ff2b09d1fd86768554ac053eeed7566bba6b488
lcoombe <lauren.e.coombe@gmail.com>
2023-12-19 09:09:38 -0800

    Refactor software.py, update software-content.html

--- python/software.py
+++ python/software.py
@@ -1,63 +1,83 @@
 #!/usr/bin/env python
+'''
+Generate and update the software-content.html file for birollab.ca
+'''
+import glob
+import os
+import sys
 
-import glob, os
+# Defining different html elements
+START = '<td><img class="software-logo" src="assets/logos/'
+P1 = '"><span class="software-title">'
+P2 = ' <a href="'
+P3 = '"><img class="git" src="assets/githubicon.svg"></a></span>'
 
-os.chdir("../softwareblurbs")
-software = open("../_includes/software-content.html", "w+")
+DIV_START = '<div style="text-align:center;"><div class="downloadinfo">'
+LINUXBREW_DIV = '<br>{% include linuxbrew-icon.html%}'
+CONDA_DIV = '<br>{% include bioconda-icon.html%}'
+DOCKER_DIV = '<br>{% include docker-icon.html%}'
+DIV_END = '</div></div>'
 
-start = '<td><img class="software-logo" src="assets/logos/'
-p1 = '"><span class="software-title">'
-p2 = ' <a href="'
-p3 = '"><img class="git" src="assets/githubicon.svg"></a></span>'
-
-ds = '<div style="text-align:center;"><div class="downloadinfo">'
-d1 = '<br>{% include linuxbrew-icon.html%}'
-d2 = '<br>{% include bioconda-icon.html%}'
-d3 = '<br>{% include docker-icon.html%}'
-de = '</div></div>'
-
-p4 = '<p style="padding-top:0">'
-end= '</p></td>'
-
-i = 0
+P4 = '<p style="padding-top:0">'
+END = '</p></td>'
 
 def downloads(lin, bio, dock):
-    if not lin and not bio and not dock: return ''
-    else: return ds + ((d1 + lin) if lin else '') + ((d2 + bio) if bio else '') + ((d3 + dock) if dock else '') + de
+    "Format string with installation options"
+    return_string = ""
+    if not lin and not bio and not dock:
+        return return_string
+    return_string += DIV_START
+    if lin:
+        return_string += (LINUXBREW_DIV + lin)
+    if bio:
+        return_string += (CONDA_DIV + bio)
+    if dock:
+        return_string += (DOCKER_DIV + dock)
+    return return_string + DIV_END
 
-for myfile in sorted(glob.glob("*.txt")):
-    f = open(myfile)
 
-    info = []
-    for x in range(0,8):
-        info.append(f.readline().strip())
-        if x == 8:
-            info.append(f.read().strip())
+def write_html(softwareblurbs_path):
+    "Write the software-content.html file"
+    i = 0
 
-    logofile = info[0]
-    name = info[1]
-    github = info[2]
-    linuxbrew = info[3]
-    bioconda = info[4]
-    docker = info[5]
-    desc = info[6]
+    with open(softwareblurbs_path + "/../_includes/software-content.html", "w+") as software:
+        for my_file in sorted(glob.glob("*.txt")):
+            with open(my_file, 'r') as fin:
+                lines = [line.strip() for line in fin.readlines()]
 
-    if i % 2 is 0:
-        software.write("<tr>")
+                logofile, name, github, linuxbrew, bioconda, docker, desc = lines[:7]
 
-    html = start + logofile + p1 + name + p2 + github + p3 + downloads(linuxbrew, bioconda, docker) + p4 + desc + end
-    software.write(html)
+                if i % 2 == 0:
+                    software.write("<tr>")
 
-    if i % 2 is 1:
-        software.write("</tr>")
+                html = START + logofile + P1 + name + P2 + github + P3 +\
+                    downloads(linuxbrew, bioconda, docker) + P4 + desc + END
+                software.write(html)
 
-    software.write("\n\n")
-    i += 1
+                if i % 2 == 1:
+                    software.write("</tr>")
 
-software.seek(0, os.SEEK_END)
-software.seek(software.tell()-7, os.SEEK_SET)
+                software.write("\n\n")
+                i += 1
 
-if i % 2 is 0:
-   software.write("</tr>")
+        software.seek(0, os.SEEK_END)
+        software.seek(software.tell()-7, os.SEEK_SET)
 
-software.close()
+        if i % 2 == 0:
+            software.write("</tr>")
+
+
+def main():
+    "Generate the birollab.ca software html"
+    if len(sys.argv[1:]) != 1:
+        print("Usage:", sys.argv[0], "<path to softwareblurbs directory>")
+        sys.exit()
+
+    softwareblurbs_path = sys.argv[1]
+    os.chdir(softwareblurbs_path)
+
+    write_html(softwareblurbs_path)
+
+
+if __name__ == "__main__":
+    main()
